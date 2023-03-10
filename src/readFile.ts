9f50b4ab0a2d410a757b7a47edfcaec78d81eafc
hi-date <h-date@sela.co.jp>
2023-03-10 17:28:03 +0900

    8.2.3まで

---
+++ src/readFile.ts
@@ -0,0 +1,10 @@
+const startTime = performance.now();
+
+// 第1引数：コールバック関数
+// 第2引数：数値（ミリ秒数）
+setTimeout(() => {
+    const endTime = performance.now();
+    console.log("タイマー呼ばれた");
+    console.log(`${endTime - startTime}ミリ秒かかった`);
+}, 3000);
+console.log("タイマー設定");
